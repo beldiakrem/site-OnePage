d3e426c129968a0873a348e95968bda99b3ef813
akrem <baldiakrem@gmail.com>
2021-05-23 10:41:48 +0200

    onepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./components/Layout";
+import MobileMenu from "./components/MobileMenu";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+import BlogHome from "./components/BlogHome";
+import CTAThree from "./components/CTAThree";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
+import CTAOne from "./components/CTAOne";
+import FunFact from "./components/FunFact";
+import CTATwo from "./components/CTATwo";
+import Team from "./components/Team";
+import Pricing from "./components/Pricing";
+import VideoOne from "./components/VideoOne";
+import AppScreen from "./components/AppScreen";
+import FAQ from "./components/FAQ";
+import Clients from "./components/Clients";
+import Testimonials from "./components/Testimonials";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const HomePage = () => (
+  <div>
+    <Layout pageTitle="Beldi | Home Page">
+      <Header
+        btnClass="main-nav__btn"
+        extraClassName="site-header-one__fixed-top"
+      />
+      <MobileMenu />
+      <Banner />
+      <Services />
+      {/* <CTAOne /> */}
+      <FunFact />
+      {/* <CTATwo /> */}
+      <Pricing />
+      {/* <Testimonials /> */}
+      <Clients />
+      <Team />
+      <VideoOne />
+      <AppScreen />
+      <FAQ />
+      <BlogHome />
+      <Contact />
+      {/* <CTAThree /> */}
+      <Footer />
+    </Layout>
+  </div>
+);
+
+export default HomePage;
